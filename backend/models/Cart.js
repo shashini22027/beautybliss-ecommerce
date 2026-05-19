6397f63c33b5ff1ff3161dc5fb4600ca76b2fa67
shashini22027 <shashini22027@users.noreply.github.com>
2026-05-19 19:11:25 +0530

    feat(cart): create cart model

---
+++ backend/models/Cart.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  cartItems: [{
+    product: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Product' },
+    qty: { type: Number, required: true, default: 1 }
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Cart', cartSchema);
