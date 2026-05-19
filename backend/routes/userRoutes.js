01e128beb2a9beec0a38babff59bade20879adc4
shashini22027 <shashini22027@users.noreply.github.com>
2026-05-19 19:11:35 +0530

    refactor(backend): optimize imports and clean routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', authUser);
-router.get('/profile', protect, getUserProfile);
+router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
