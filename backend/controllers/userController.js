c8e13d58f8b5d39fbc87f8a36be2a31cdf3a0417
shashini22027 <shashini22027@users.noreply.github.com>
2026-05-19 19:11:19 +0530

    feat(auth): implement user registration controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message: 'Invalid user data' });
+  }
+};
+
+module.exports = { registerUser };
