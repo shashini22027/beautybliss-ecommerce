1ba09ce343342e5c063bd4986f3e0d58c61b6404
shashini22027 <shashini22027@users.noreply.github.com>
2026-05-19 19:11:32 +0530

    feat(reviews): create review routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getProducts, getProductById, createProduct } = require('../controllers/productController');
+const { getProducts, getProductById, createProduct, createProductReview } = require('../controllers/productController');
 const { protect, admin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 module.exports = router;
