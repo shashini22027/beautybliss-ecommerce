22aff9668b1b03d5934101008ec129ac97e75aa7
shashini22027 <shashini22027@users.noreply.github.com>
2026-05-19 19:11:26 +0530

    feat(cart): implement cart controllers

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,38 @@
+const Cart = require('../models/Cart');
+
+const getCart = async (req, res) => {
+  let cart = await Cart.findOne({ user: req.user._id }).populate('cartItems.product');
+  if (!cart) {
+    cart = await Cart.create({ user: req.user._id, cartItems: [] });
+  }
+  res.json(cart);
+};
+
+const addToCart = async (req, res) => {
+  const { productId, qty } = req.body;
+  let cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    cart = new Cart({ user: req.user._id, cartItems: [] });
+  }
+
+  const existItemIndex = cart.cartItems.findIndex(x => x.product.toString() === productId);
+  if (existItemIndex > -1) {
+    cart.cartItems[existItemIndex].qty = qty;
+  } else {
+    cart.cartItems.push({ product: productId, qty });
+  }
+
+  await cart.save();
+  res.status(201).json(cart);
+};
+
+const removeFromCart = async (req, res) => {
+  let cart = await Cart.findOne({ user: req.user._id });
+  if (cart) {
+    cart.cartItems = cart.cartItems.filter(x => x.product.toString() !== req.params.id);
+    await cart.save();
+  }
+  res.json(cart);
+};
+
+module.exports = { getCart, addToCart, removeFromCart };
