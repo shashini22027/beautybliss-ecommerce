04985529c468cae0fee3f2a880731e749ef8b737
shashini <shashiniruwanthika13@gmail.com>
2026-06-18 15:58:21 +0530

    fix: bind server to 0.0.0.0 for deployment

--- backend/index.js
+++ backend/index.js
@@ -106,4 +106,6 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0' (), () => {
+  console.log(`Server running on port ${PORT}`);
+});
