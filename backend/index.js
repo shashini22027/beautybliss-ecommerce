93df17edf20740105391931bb73ce8665b7cd531
shashini <shashiniruwanthika13@gmail.com>
2026-06-18 16:04:37 +0530

    fix: corrected app listen syntax

--- backend/index.js
+++ backend/index.js
@@ -106,6 +106,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0.0.0.0' (), () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
